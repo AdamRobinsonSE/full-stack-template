a8c669795b5b8c6775b6e337d470a32ee4e76647
Adam Robinson <robinson.db@gmail.com>
2022-06-25 10:53:00 -0400

    added variables and mongo connection

--- server.js
+++ server.js
@@ -4,3 +4,15 @@ const cors = require('cors')    // Import cors, a middleware that allows cross-o
 const MongoClient = require('mongodb').MongoClient  // Import mongodb, a MongoDB database
 require('dotenv').config()   // Import dotenv, a module that loads environment variables from a .env file, hides them from the source code
 const PORT = process.env.PORT || 8000   // Set port, a port number to listen on
+
+let db,
+    dbConnectionString = process.env.DB_STRING, // Set dbConnectionString, a string that holds the connection string to the database, grabs the variable from the .env file
+    dbName = 'star-trek-api' // Set dbName, a string that holds the name of the database',
+    collection
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log('Connected to database')    // Log to console that we connected to the database
+        db = client.db(dbName)  // Set db, a variable that holds the database
+        collection = db.collection('alien-info')    // Set collection, a variable that holds the collection of the database
+    })
