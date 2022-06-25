89e7c068ed428ba840ea45c620a8abf37a65f10b
Adam Robinson <robinson.db@gmail.com>
2022-06-25 11:26:56 -0400

    added port and mongo connections

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express()           // Create express app, a web framework for node.
 const cors = require('cors')    // Import cors, a middleware that allows cross-origin requests, bypass cross-origin errors
 const MongoClient = require('mongodb').MongoClient  // Import mongodb, a MongoDB database
 require('dotenv').config()   // Import dotenv, a module that loads environment variables from a .env file, hides them from the source code
-const PORT = process.env.PORT || 8000   // Set port, a port number to listen on
+
 
 let db,
     dbConnectionString = process.env.DB_STRING, // Set dbConnectionString, a string that holds the connection string to the database, grabs the variable from the .env file
-    dbName = 'star-trek-api' // Set dbName, a string that holds the name of the database',
+    dbName = 'star-trek-api', // Set dbName, a string that holds the name of the database',
     collection
 
 MongoClient.connect(dbConnectionString)
@@ -16,3 +16,7 @@ MongoClient.connect(dbConnectionString)
         db = client.db(dbName)  // Set db, a variable that holds the database
         collection = db.collection('alien-info')    // Set collection, a variable that holds the collection of the database
     })
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server listening on port`) // Log to console that the server is listening on port 8000
+})
