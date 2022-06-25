af4e486d32476e498095203aa87bae1492430413
Adam Robinson <robinson.db@gmail.com>
2022-06-25 12:30:36 -0400

    added endpoint in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express') // Import express, a web framework for node.js, does all the heavy lifting for us
 const app = express()           // Create express app, a web framework for node.js, a variable that holds the express app
 const cors = require('cors')    // Import cors, a middleware that allows cross-origin requests, bypass cross-origin errors
+const { response } = require('express')
 const MongoClient = require('mongodb').MongoClient  // Import mongodb, a MongoDB database
 require('dotenv').config()   // Import dotenv, a module that loads environment variables from a .env file, hides them from the source code
 
@@ -23,6 +24,14 @@ app.use(express.static('public')) // Use express.static, a middleware that serve
 app.use(express.json()) // Use express.json, a middleware that parses JSON, allows us to use JSON in our code
 app.use(express.urlencoded({ extended: true })) // Use express.urlencoded, a middleware that parses URL encoded data, allows us to use URL encoded data in our code
 
+app.get('/', async (req, res) => {
+    try {
+        res.render('index') // Render the index.ejs file, a file that holds the html for the index page
+    }
+    catch (error) {
+        response.status(500).send({message: error.message}) // Send a 500 error if there is an error, and send the error message
+    }
+})
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server listening on port`) // Log to console that the server is listening on port
